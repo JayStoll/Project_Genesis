c6670da44c8e2e3a301618368b789f2810d69892
Jayden Stoll <jstoll11@gmail.com>
2018-03-06 20:31:50 -0700

    re-done of repository

---
+++ Source/Project Genesis Source/CreateInvoicePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_Genesis_Source
+{
+    /// <summary>
+    /// Interaction logic for CreateInvoicePage.xaml
+    /// </summary>
+    public partial class CreateInvoicePage : Window
+    {
+        public CreateInvoicePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        //Connects straight away to the main page
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            this.Hide();
+        }
+    }
+}
