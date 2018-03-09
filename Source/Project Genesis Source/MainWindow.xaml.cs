d3ed0de7d5260bd033f2d29ac31da4c26f81aa48
Jayden Stoll <jstoll11@gmail.com>
2018-03-08 21:29:53 -0700

    Add the DatabaseConnection class

--- Source/Project Genesis Source/MainWindow.xaml.cs
+++ Source/Project Genesis Source/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace Project_Genesis_Source
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
