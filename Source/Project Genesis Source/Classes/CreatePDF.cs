4793450eb76a2ec8decd40578051869bbe166511
Jayden Stoll <jstoll11@gmail.com>
2018-04-10 12:15:45 -0600

    Minor UI bug fixes

--- Source/Project Genesis Source/Classes/CreatePDF.cs
+++ Source/Project Genesis Source/Classes/CreatePDF.cs
@@ -184,7 +184,7 @@ namespace Project_Genesis_Source.Classes {
             #endregion
 
             // set the file to print the document
-            string filename = @"invoices\" + client.ClientFName + " " + client.ClientLName + @"\" + client.ClientFName + client.ClientLName + DateTime.Now.ToString("dd/mm/yyyy") + ".pdf";
+            string filename = @"..\invoices\" + client.ClientFName + " " + client.ClientLName + @"\" + client.ClientFName + client.ClientLName + DateTime.Now.ToString("dd/mm/yyyy") + ".pdf";
             // creates a new directory if one is not present
             System.IO.FileInfo file = new System.IO.FileInfo(filename);
             file.Directory.Create();  // Skips this if the file path is found
