ada181b93a907a8fc56a716e19a503a720675874
Jayden Stoll <jstoll11@gmail.com>
2018-04-14 13:38:42 -0600

    began start of backup

--- Source/Project Genesis Source/Backup1.xaml.cs
+++ Source/Project Genesis Source/Backup1.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Project_Genesis_Source.Classes;
+using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Project_Genesis_Source
 {
@@ -20,9 +12,38 @@ namespace Project_Genesis_Source
     /// </summary>
     public partial class Backup1 : Page
     {
+        DatabaseConnection dataConn = new DatabaseConnection();
+
+        string dbName = "GenesisDB.mdf";
+        SqlConnection sql = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable data = new DataTable();
+
         public Backup1()
         {
             InitializeComponent();
+        }
+
+        private void BackupDatabase(object sender, RoutedEventArgs e) {
+            sql.ConnectionString = dataConn.connString;
+
+            string backupDestination = @"CAJNBackup\";
+
+            if (!System.IO.Directory.Exists(backupDestination))     System.IO.Directory.CreateDirectory(backupDestination);
+
+            try {
+                sql.Open();
+                command = new SqlCommand("backup database " + dbName + " to disk='" + backupDestination + DateTime.Now.ToString("ddMMyyyy_HHmmss") + ".Bak'", sql);
+                command.ExecuteNonQuery();
+                sql.Close();
+                MessageBox.Show("Backup successful!");
+            }
+#pragma warning disable CS0168 // Variable is declared but never used
+            catch (Exception ex){
+#pragma warning restore CS0168 // Variable is declared but never used
+                MessageBox.Show("error during backup");
+            }
         }
     }
 }
