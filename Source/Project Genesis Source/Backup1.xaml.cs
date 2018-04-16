209458f4bcbf8e5600ae14b1f0886e4273411ad4
Jayden Stoll <jstoll11@gmail.com>
2018-04-16 14:25:38 -0600

    backup and recovery done

--- Source/Project Genesis Source/Backup1.xaml.cs
+++ Source/Project Genesis Source/Backup1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Project_Genesis_Source.Classes;
+﻿using Microsoft.Win32;
+using Project_Genesis_Source.Classes;
 using System;
 using System.Data;
 using System.Data.SqlClient;
@@ -14,7 +15,12 @@ namespace Project_Genesis_Source
     {
         DatabaseConnection dataConn = new DatabaseConnection();
 
-        string dbName = "GenesisDB.mdf";
+        // get the path of the database
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string dbName = path + @"\CAJNData\GenesisDB.mdf";
+        string backupDestination = path + @"\CAJNBackup\";
+
+        // SQL manipulation
         SqlConnection sql = new SqlConnection();
         SqlCommand command = new SqlCommand();
         SqlDataAdapter adapter = new SqlDataAdapter();
@@ -28,21 +34,45 @@ namespace Project_Genesis_Source
         private void BackupDatabase(object sender, RoutedEventArgs e) {
             sql.ConnectionString = dataConn.connString;
 
-            string backupDestination = @"CAJNBackup\";
+            
 
+            // create backup directory if it doesn't exsist
             if (!System.IO.Directory.Exists(backupDestination))     System.IO.Directory.CreateDirectory(backupDestination);
 
             try {
                 sql.Open();
-                command = new SqlCommand("backup database " + dbName + " to disk='" + backupDestination + DateTime.Now.ToString("ddMMyyyy_HHmmss") + ".Bak'", sql);
+                // creates the backup file
+                command = new SqlCommand("BACKUP DATABASE [" + dbName + "] TO DISK='" + backupDestination + DateTime.Now.ToString("dd-MM-yyyy") + ".bak'", sql);
                 command.ExecuteNonQuery();
                 sql.Close();
                 MessageBox.Show("Backup successful!");
             }
-#pragma warning disable CS0168 // Variable is declared but never used
             catch (Exception ex){
-#pragma warning restore CS0168 // Variable is declared but never used
-                MessageBox.Show("error during backup");
+                MessageBox.Show(ex.Message + " " + command.CommandText);
+            }
+        }
+
+        private void RecoverData(object sender, RoutedEventArgs e) {
+            sql.ConnectionString = dataConn.connString;
+
+            try {
+                OpenFileDialog file = new OpenFileDialog();
+                file.Filter = "Backup File | *.bak";
+                if (file.ShowDialog() == true) {
+                    // creates the backup file
+                    string sqlCom = "ALTER DATABASE [" + dbName + "] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;";
+                    sqlCom += "RESTORE DATABASE [" + dbName + "] FROM DISK='" + file.FileName + "' WITH REPLACE";
+                    SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB; Initial Catalog=master;Integrated Security=True");
+                    command = new SqlCommand(sqlCom, con);
+
+                    con.Open();
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Recover successful!");
+                    con.Close();
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
             }
         }
     }
